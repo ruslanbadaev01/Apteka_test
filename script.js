508418a9c61c7fa8c60f0079ace2e5f75af3c2c6
L0c_d0G <94112336+ruslanbadaev01@users.noreply.github.com>
2022-07-16 16:51:46 +0300

    carusel

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function viewDiv(){
+  document.getElementById('t1').style.display = "block";
+};
+function unviewDiv(){
+  document.getElementById('t1').style.display = "none";
+};
+  
+function viewDiv2(){
+  document.getElementById('t2').style.display = "block";
+};
+function unviewDiv2(){
+  document.getElementById('t2').style.display = "none";
+};
+
+function viewDiv3(){
+  document.getElementById('t3').style.display = "block";
+};
+function unviewDiv3(){
+  document.getElementById('t3').style.display = "none";
+};
